dd7cd43f4a3695364e9fedc724542b1ce3552b78
RajAakash <https://github.com/rajAakash01>
2024-04-16 15:41:19 -0500

    AppEmbeddings data added

--- data_preparation/averageLabel.py
+++ data_preparation/averageLabel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the data from a CSV file
-data = pd.read_csv('../appEmbeddings/appEmbeddings10_5.csv')
+data = pd.read_csv('C:/Users/ACER/Downloads/averageAppEmbeddings.csv')
 
 # Group the data by the label(s) and calculate the mean of each group
 grouped_data = data.groupby('app').mean()
@@ -10,5 +10,5 @@ grouped_data = data.groupby('app').mean()
 grouped_data = grouped_data.reset_index()
 
 # Save the result to a new CSV file or display it
-grouped_data.to_csv('../appEmbeddings/averaged_data.csv', index=False)
+grouped_data.to_csv('C:/Users/ACER/Downloads/averageDataLabel.csv', index=False)
 print(grouped_data)
